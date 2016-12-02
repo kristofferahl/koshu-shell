ca35c1dd10505206b648ed962e9d097e2e854ce7
Kristoffer Ahl <git@77dynamite.com>
2016-12-02 13:31:56 +0100

    - Renamed aliases set by env.sh.

--- env.sh
+++ env.sh
@@ -3,9 +3,9 @@
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $here || exit
 
-echo "alias test='$here/src/koshu.sh test -f $here/koshufile'"
-echo "alias watch='watch -p \"**/*\" -c \"$here/src/koshu.sh test -f $here/koshufile\"'"
-echo "alias koshudev='$here/src/koshu.sh'"
+echo "alias koshu-test='$here/src/koshu.sh test -f $here/koshufile'"
+echo "alias koshu-watch='watch -p \"**/*\" -c \"$here/src/koshu.sh test -f $here/koshufile\"'"
+echo "alias koshu-dev='$here/src/koshu.sh'"
 echo
 echo "# Run this command to configure your shell:"
 echo "# eval \"\$(./env.sh)\""
