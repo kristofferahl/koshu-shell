d649d2d3d4dcedc4ccf817838f73aade9a599525
Kristoffer Ahl <git@77dynamite.com>
2016-12-02 09:42:51 +0100

    - Added koshudev alias to env.sh.

--- env.sh
+++ env.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $here
+cd $here || exit
 
 echo "alias test='$here/src/koshu.sh test --file $here/koshufile'"
+echo "alias koshudev='$here/src/koshu.sh'"
 echo "# Run this command to configure your shell:"
 echo "# eval \"\$(./env.sh)\""
