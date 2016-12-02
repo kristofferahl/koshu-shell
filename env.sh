7a6c0f1fe19014953980619388829d5fcfd8d304
Kristoffer Ahl <git@77dynamite.com>
2016-12-02 13:09:07 +0100

    - Added a watch command to env.sh.

--- env.sh
+++ env.sh
@@ -3,7 +3,9 @@
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $here || exit
 
-echo "alias test='$here/src/koshu.sh test --file $here/koshufile'"
+echo "alias test='$here/src/koshu.sh test -f $here/koshufile'"
+echo "alias watch='watch -p \"**/*\" -c \"$here/src/koshu.sh test -f $here/koshufile\"'"
 echo "alias koshudev='$here/src/koshu.sh'"
+echo
 echo "# Run this command to configure your shell:"
 echo "# eval \"\$(./env.sh)\""
